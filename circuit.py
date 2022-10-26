256f7e0696e5954521a83347978aed580993fdf3
Marcela Soriano <97627826+Marcelasoriano@users.noreply.github.com>
2022-10-25 20:26:34 -0400

    Add files via upload

---
+++ circuit.py
@@ -0,0 +1,8 @@
+#Name: Marcela Soriano Cornejo
+#Email: marcela.sorianocornejo40@myhunter.cuny.edu
+#Date: 25 October 2022
+#Program 26: This code creates an output situation.
+
+out1 = (in1 and in1)
+out2 = ((not in1 and in2) or (in1 and not in2))
+out3 = (not in2)
